d96fc670dfdfc6325a2ebb3a0c13c18cfe174696
José Carlos <etijcvivi@gmail.com>
2022-07-08 20:04:20 -0300

    Criação Componente page-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PageHomeNavbarComponent } from './components/page-home-navbar/page-home-navbar.component';
 import { PageHomeCarosselComponent } from './components/page-home-carossel/page-home-carossel.component';
 import { PageHomeCardsComponent } from './components/page-home-cards/page-home-cards.component';
+import { PageLoginComponent } from './components/page-login/page-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageHomeNavbarComponent,
     PageHomeCarosselComponent,
-    PageHomeCardsComponent
+    PageHomeCardsComponent,
+    PageLoginComponent
   ],
   imports: [
     BrowserModule
