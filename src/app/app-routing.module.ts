cc179ed4b849fe311752f72b97fffd4260204ce5
José Carlos <etijcvivi@gmail.com>
2022-07-11 00:47:38 -0300

    criação de componentes, RouterLink, HttpClient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PageHomeCarosselComponent} from './components/page-home-carossel/page-home-carossel.component';
 import {PageLoginComponent} from './components/page-login/page-login.component';
-import {PageUsuarioLogadoComponent} from './components/page-usuario-logado/page-usuario-logado.component'
+import {PageUsuarioLogadoComponent} from './components/page-usuario-logado/page-usuario-logado.component';
+import {PageHomeNavbarComponent} from './components/page-home-navbar/page-home-navbar.component';
+import {PageAdminNavbarComponent} from './components/page-admin-navbar/page-admin-navbar.component';
+import {PageConsultaCardsComponent} from './components/page-consulta-cards/page-consulta-cards.component';
 
 const routes: Routes = [
    {
     path: '',
-    component: PageHomeCarosselComponent,
+    component: PageHomeNavbarComponent,
     children: [
-      {path: 'login', component: PageLoginComponent}
+      {path: '', component: PageHomeCarosselComponent},
+      {path: 'login', component: PageLoginComponent},
+      {path: 'game/:id', component: PageConsultaCardsComponent}
     ]
   },
-   { path: 'logado', component: PageUsuarioLogadoComponent, pathMatch: 'full' }
+
+
+
+   {
+    path: 'admin',
+    component: PageAdminNavbarComponent,
+    children: [
+      {path: '', component: PageUsuarioLogadoComponent}
+    ]
+  }
 ];
 
 @NgModule({
