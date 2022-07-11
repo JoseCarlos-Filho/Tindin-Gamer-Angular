b2669d22058b49eb3da7fadf72e70dd7b44a7d1e
José Carlos <etijcvivi@gmail.com>
2022-07-10 22:34:16 -0300

    criação de componentes, RouterLink, HttpClient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PageHomeCarosselComponent} from './components/page-home-carossel/page-home-carossel.component';
 import {PageLoginComponent} from './components/page-login/page-login.component';
+import {PageUsuarioLogadoComponent} from './components/page-usuario-logado/page-usuario-logado.component'
 
 const routes: Routes = [
-   { path: '', component: PageHomeCarosselComponent },
-   { path: 'login', component: PageLoginComponent }
+   {
+    path: '',
+    component: PageHomeCarosselComponent,
+    children: [
+      {path: 'login', component: PageLoginComponent}
+    ]
+  },
+   { path: 'logado', component: PageUsuarioLogadoComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
