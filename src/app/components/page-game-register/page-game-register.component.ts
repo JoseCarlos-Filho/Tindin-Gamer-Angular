c5b32ede5da872cd3fb0d5bb92ea95b2c286d8ff
José Carlos <etijcvivi@gmail.com>
2022-07-17 20:48:33 -0300

    Criando rotas e conexão com componente page-register

--- src/app/components/page-game-register/page-game-register.component.ts
+++ src/app/components/page-game-register/page-game-register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { Game } from '../../interfaces/game';
+import { GameService } from '../../services/game.service';
 @Component({
   selector: 'app-page-game-register',
   templateUrl: './page-game-register.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageGameRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private gameService: GameService) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(): void {
+    //this.login.email = this.loginForm.value.email?.toString();
+    //this.login.password = this.loginForm.value.password?.toString();
+    const game: Game = {
+      title: this.registerForm.value.title?.toString(),
+      description: this.registerForm.value.description?.toString(),
+    };
+    // Process checkout data here
+    let algumacoisa = this.loginService.login(login).subscribe(login =>{
+      console.log(login)
+    })
+    console.log(algumacoisa);
+    //console.warn('Your order has been submitted', this.loginForm.value);
+    //this.loginForm.reset();
+  }
+
 
 }
